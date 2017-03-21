54d755726ca41aac9c11da63f7bf7817048dfe9a
Alicia Mastretta-Yanes <a.mstt.yanes@gmail.com>
2017-03-21 12:56:16 -0600

    added echo to example

--- Unidad5/Prac_Uni5/FastqsEjemplo/bin/1cleaning.sh
+++ Unidad5/Prac_Uni5/FastqsEjemplo/bin/1cleaning.sh
@@ -6,12 +6,14 @@
 pmisdatos=/Users/ticatla/hubiC/Science/Teaching/Mx/BioinfInvgRepro/BioinfInvRepro2017-II/Unidad5/Prac_Uni5/FastqsEjemplo/datos/
 
 ## Clean data trimming and filtering low quality seqs
+echo "Comenzar la limpieza" #Hago el echo para saber donde va mi script cuando lo corro
 docker run --rm -v $pmisdatos:/data biocontainers/fastxtools /bin/bash -c "fastx_trimmer -f 1 -l 70 -i human_Illumina_dataset.fastq -v | \
-fastq_quality_filter -q 20 -p 80 -o clean_human_data.fastq -v"
+fastq_quality_filter -q 20 -p 90 -o clean_human_data.fastq -v"
 
 # en el comando anterior:
 # --rm: borra el contenedor después de que termina de correr.
 # /bin/bash -c "<comandos>" sirve para poder utilizar varios comandos unidos con | o & dentro del mismo contenedor
 
 ## Create fastqc report before and after
+echo "Hacer fastqc analisis"
 docker run --rm -v $pmisdatos:/data biocontainers/fastqc fastqc human_Illumina_dataset.fastq clean_human_data.fastq
